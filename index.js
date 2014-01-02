b040f5213f65617da611454b0ad1336447893816
James Halliday <mail@substack.net>
2014-01-02 01:40:38 -0800

    add more points to the polygon with less points

--- index.js
+++ index.js
@@ -21,11 +21,20 @@ function Morph (a, b) {
     pc.setAttribute('style', 'fill:none;stroke:purple;stroke-width:1px;');
     pc.setAttribute('d', 'M ' + a.join(' L '));
     
-    var start = 'M ' + a.slice(0, Math.min(a.length, b.length)).join(' L ');
-    var end = 'M ' + b.slice(0, Math.min(a.length, b.length)).join(' L ');
+    var auga = a;
+    var augb = b;
+    if (a.length < b.length) {
+        auga = expand(a, b.length);
+    }
+    else if (b.length < a.length) {
+        augb = expand(b, a.length);
+    }
+    
+    var start = 'M ' + auga.join(' L ');
+    var end = 'M ' + augb.join(' L ');
     
     var anim = createElement('animate');
-    anim.setAttribute('dur', '5s');
+    anim.setAttribute('dur', '3s');
     anim.setAttribute('repeatCount', 'indefinite');
     anim.setAttribute('attributeName', 'd');
     anim.setAttribute('d', start);
@@ -56,4 +65,46 @@ function mean (pts) {
         sum[1] += pts[i][1];
     }
     return [ sum[0] / sum.length, sum[1] / sum.length ];
+}
+
+function distances (pts) {
+    var res = [];
+    for (var i = 0; i < pts.length - 1; i++) {
+        res.push({ index: i, distance: dist(pts[i], pts[i+1]) });
+    }
+    return res;
+}
+
+function dist (a, b) {
+    var x = a[0] - b[0], y = a[1] - b[1];
+    return Math.sqrt(x*x + y*y);
+}
+
+
+function expand (a, len) {
+    var res = a.slice();
+    var extra = [];
+    var da = distances(a).sort(cmp);
+    
+    for (var i = 0; i < len - a.length; i++) {
+        if (da.length === 0) {
+            da = distances(res).sort(cmp);
+        }
+        var d = da.shift();
+        res.splice(d.index, 0, mean([ a[d.index], a[d.index+1] ]));
+        
+        for (var j = d.index; j < da.length; j++) {
+            da[j].index ++;
+        }
+    }
+    var offset = 0;
+    for (var i in extra) {
+        res.splice(i + offset, 0, extra[i]);
+        offset ++;
+    }
+    return res;
+    
+    function cmp (a, b) {
+        return a.distance > b.distance ? -1 : 1;
+    }
 }
